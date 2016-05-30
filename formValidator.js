3d034446da40fbd8ccc718db801f38a48eb0ed57
Gaël Blanchemain <gael@gr0wing.com>
2016-05-30 13:38:30 -0400

    Functional, still very wet.

--- formValidator.js
+++ formValidator.js
@@ -6,44 +6,67 @@ var clickConfirmed = function(e) {
 e.preventDefault();
 $('#myForm').find('input').each( function(){
 var dataValidationType = $(this).attr('data-validate');
+var warningDataType = "";
+if (dataValidationType == 'wordcount'){
+	warningDataType = "words";
+} else {
+	warningDataType = "characters";
+}
 var minLength = $(this).attr('minLength');
+var warningMinLength = "AT LEAST"+" "+minLength+" "+warningDataType+" "+'long';
+if (minLength === undefined) {minLength = ""; warningMinLength = "";}
 var maxLength = $(this).attr('data-validate-max');
+var warningMaxLength = "and no longer than "+maxLength+" "+warningDataType;
+if (maxLength === undefined) {maxLength = ""; warningMaxLength = "";}
 var required = $(this).attr('data-requirement');
 var fieldContent = $(this).val();
-var formPassCriteria = true;
-     if(required == 'required') {
-				 switch (dataValidationType) {
-				    case 'charcount':
-				    
-			var filter = /^[A-Za-z/]{2,}$/;
-
-//if submission meets criteria > customize .alert and
- 
-			 if (!filter.test(fieldContent)) {
-			 	   window.setTimeout(function() {
+var fieldName = $(this).attr('name');
+var alert = '<div class="alert alert-warning alert-dismissible"></a>My friend, your '+ fieldName+" "+"should be"+" "+" "+ warningMinLength+" "+ warningMaxLength+'.</div>';
+var failsTestWarning = function(alert){window.setTimeout(function() {
       $(".alert").fadeTo(1500, 0).slideUp(500, function(){
         $(this).remove(); 
       });
     }, 4000);
-			 var alert = '<div class="alert alert-warning alert-dismissible"></a>My friend, your first name should be AT LEAST 2 characters long...No?</div>';
 			 	$('#form-group').prepend(alert);
-			 	formPassCriteria = false;
+			 	};
+var formPassCriteria = true;
+if(required == 'required') {
+				 switch (dataValidationType) {			 	
+				   case 'charcount':
+				   var filter = new RegExp("[\\w]{"+minLength+","+maxLength+"}");
+			 if (!filter.test(fieldContent)) {
+			 	   failsTestWarning(alert);
+			 	   formPassCriteria = false;
 				return false;
 				}else{
 				formPassCriteria = true;
 				}
 				     break;
 				    case 'numeric':
-				       console.log(dataValidationType);
+				    //warningFieldType = "numbers only, ";
+				      filter = new RegExp("[\\d]{"+minLength+","+maxLength+"}");
+			 if (!filter.test(fieldContent)) {
+			 	failsTestWarning(alert);
+			 	formPassCriteria = false;
+				return false;
+				}else{
+				formPassCriteria = true;
+				}
 				        break;
 				    case 'wordcount':
-				        console.log(dataValidationType);
+				       filter = new RegExp("^(?:\\b\\w+\\b[\\s\\r\\n]*){"+minLength+","+maxLength+"}$");
+				       if (!filter.test(fieldContent)) {
+			 	  failsTestWarning(alert);
+			 	  formPassCriteria = false;
+				return false;
+				}else{
+				formPassCriteria = true;
+				}
 				        break;
 				} 	
 	 }else{
 	 	return;
 	 }
-
 }
 //closes $('#myform').find('data-validate').each(
 );
